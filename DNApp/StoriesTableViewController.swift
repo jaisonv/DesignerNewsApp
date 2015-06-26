5d3db2543b6972ba71bb039230e17c5cc4894bbe
Jaison Vieira <jaisonnvieira@gmail.com>
2015-06-26 01:13:53 -0300

    Creating a delegate

--- DNApp/StoriesTableViewController.swift
+++ DNApp/StoriesTableViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class StoriesTableViewController: UITableViewController {
+class StoriesTableViewController: UITableViewController, StoryTableViewCellDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -43,6 +43,8 @@ class StoriesTableViewController: UITableViewController {
         cell.upvoteButton.setTitle("59", forState: UIControlState.Normal)
         cell.commentButton.setTitle("32", forState: UIControlState.Normal)
         
+        cell.delegate = self
+        
         return cell
     }
     
@@ -52,4 +54,13 @@ class StoriesTableViewController: UITableViewController {
         tableView.deselectRowAtIndexPath(indexPath, animated: true)
     }
 
+    // MARK: StoryTableViewCellDelegate
+    
+    func storyTableViewCellDidTouchUpvote(cell: StoryTableViewCell, sender: AnyObject) {
+        // TODO: implement upvote
+    }
+    
+    func storyTableViewCellDidTouchComment(cell: StoryTableViewCell, sender: AnyObject) {
+        performSegueWithIdentifier("CommentsSegue", sender: self)
+    }
 }
--- DNApp/StoryTableViewCell.swift
+++ DNApp/StoryTableViewCell.swift
@@ -9,6 +9,11 @@
 import UIKit
 import Spring
 
+protocol StoryTableViewCellDelegate: class {
+    func storyTableViewCellDidTouchUpvote(cell: StoryTableViewCell, sender: AnyObject)
+    func storyTableViewCellDidTouchComment(cell: StoryTableViewCell, sender: AnyObject)
+}
+
 class StoryTableViewCell: UITableViewCell {
 
     @IBOutlet weak var badgeImageView: UIImageView!
@@ -18,16 +23,21 @@ class StoryTableViewCell: UITableViewCell {
     @IBOutlet weak var authorLabel: UILabel!
     @IBOutlet weak var upvoteButton: SpringButton!
     @IBOutlet weak var commentButton: SpringButton!
+    weak var delegate: StoryTableViewCellDelegate?
     
     @IBAction func upvoteButtonDidTouch(sender: AnyObject) {
         upvoteButton.animation = "pop"
         upvoteButton.force = 3
         upvoteButton.animate()
+        
+        delegate?.storyTableViewCellDidTouchUpvote(self, sender: sender)
     }
     
     @IBAction func commentButtonDidTouch(sender: AnyObject) {
         commentButton.animation = "pop"
         commentButton.force = 3
         commentButton.animate()
+        
+        delegate?.storyTableViewCellDidTouchComment(self, sender: sender)
     }
 }
