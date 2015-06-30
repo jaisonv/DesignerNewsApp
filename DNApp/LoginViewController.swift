5b4da77f11fc718bdacd5a57363982eac0684d7d
Jaison Vieira <jaisonnvieira@gmail.com>
2015-06-30 19:57:31 -0300

    Keyboard Interactions And Animation

--- DNApp/LoginViewController.swift
+++ DNApp/LoginViewController.swift
@@ -9,14 +9,19 @@
 import UIKit
 import Spring
 
-class LoginViewController: UIViewController {
+class LoginViewController: UIViewController, UITextFieldDelegate {
 
     @IBOutlet weak var dialogView: DesignableView!
+    @IBOutlet weak var emailImageView: SpringImageView!
+    @IBOutlet weak var passwordImageView: SpringImageView!
+    @IBOutlet weak var emailTextField: DesignableTextField!
+    @IBOutlet weak var passwordTextField: DesignableTextField!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        emailTextField.delegate = self
+        passwordTextField.delegate = self
     }
 
     @IBAction func loginButtonTouch(sender: AnyObject) {
@@ -26,5 +31,30 @@ class LoginViewController: UIViewController {
 
     @IBAction func closeButtonDidTouch(sender: AnyObject) {
         dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    override func touchesBegan(touches: Set<NSObject>, withEvent event: UIEvent) {
+        view.endEditing(true)
+    }
+    
+    func textFieldDidBeginEditing(textField: UITextField) {
+        if textField == emailTextField {
+            emailImageView.image = UIImage(named: "icon-mail-active")
+            emailImageView.animate()
+        } else {
+            emailImageView.image = UIImage(named: "icon-mail")
+        }
+        
+        if textField == passwordTextField {
+            passwordImageView.image = UIImage(named: "icon-password-active")
+            passwordImageView.animate()
+        } else {
+            passwordImageView.image = UIImage(named: "icon-password")
+        }
+    }
+    
+    func textFieldDidEndEditing(textField: UITextField) {
+        emailImageView.image = UIImage(named: "icon-mail")
+        passwordImageView.image = UIImage(named: "icon-password")
     }
 }
